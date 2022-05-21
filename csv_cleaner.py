2233846d39ac76cc00cec150f417fc24c7700734
Franky <cuifranky95@gmail.com>
2022-05-21 17:44:49 -0400

    added headers

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -6,6 +6,11 @@ with open('Meme Studies Index .xlsx - Sheet1.csv', newline='', encoding='utf-8')
     reader = csv.DictReader(csvfile)
     with open('Meme Studies Index Cleaned.csv', 'w', newline='', encoding='utf-8') as authors:
         writer = csv.writer(authors)
+
+        row_list = ['Author1 Last name','Author1 First name','Author2 Last name','Author2 First name','Author3 Last name','Author3 First name','Author4 Last name','Author4 First name','Author5 Last name','Author5 First name','Author6 Last name','Author6 First name','Author7 Last name','Author7 First name','Year*','Scholarly Article*','Platform','Place 1','Place 2','Resource Type*','Book','Book edited by','Publisher','ISBN','City (if book)','Volume','Number','Pages','Link (Where Applicable)','Topic1*','Topic2*','Topic3','Topic4']
+
+        writer.writerow(row_list)
+
         
         #iterate through every line of csv file
         for row in reader:
