95ec8536ba2955d5c9be4cd8b2386454a8c2c542
jirrian <jillianzhong@yahoo.com>
2022-06-16 16:23:47 -0400

    updated cleaner script to fix isbn cleaning and strip whitespaces at beginning and end of strings, dled new meme index sheet and generated new csvs

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -14,7 +14,7 @@ with open('Meme Studies Index .xlsx - Sheet1.csv', newline='', encoding='utf-8')
         
         #iterate through every line of csv file
         for row in reader:
-            cleaned_isbn = re.sub('[^0-9]", ','',row['ISBN'])
+            cleaned_isbn = re.sub("[^0-9]", "",row['ISBN'])
             article_name = row['Scholarly Article*'].replace('“','"').replace('”','"').replace("’","'").replace("‘","'").replace('–','-').replace('…','...')
 
             cleaned_pages = row['Pages'].replace('–','-')
@@ -26,7 +26,14 @@ with open('Meme Studies Index .xlsx - Sheet1.csv', newline='', encoding='utf-8')
                 cleaned_link = link_str
             row_list = [row['Author1 Last name']] + [row['Author1 First name']] + [row['Author2 Last name']] + [row['Author2 First name']] + [row['Author3 Last name']] + [row['Author3 First name']] + [row['Author4 Last name']] + [row['Author4 First name']] + [row['Author5 Last name']] + [row['Author5 First name']] + [row['Author6 Last name']] + [row['Author6 First name']] + [row['Author7 Last name']] + [row['Author7 First name']] + [row['Year*']] + [article_name] + [row['Platform']] + [row['Place 1']] + [row['Place 2']] + [row['Resource Type*']] + [row['Book']] + [row['Book edited by']] + [row['Publisher']] + [cleaned_isbn] + [row['City (if book)']] + [row['Volume']] + [row['Number']] + [cleaned_pages] + [cleaned_link] + [row['Topic1*']] + [row['Topic2*']] + [row['Topic3']] + [row['Topic4']]
 
-            writer.writerow(row_list)
+            row_list_stripped = []
+            for item in row_list:
+                if item:
+                    row_list_stripped.append(item.strip())
+                else:
+                    row_list_stripped.append(item)
+
+            writer.writerow(row_list_stripped)
 
 
 
